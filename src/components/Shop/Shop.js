14587c6dce6105b0ff30a47e03ad54e2c5ecb079
sowmik-sec <sowmik.sec@gmail.com>
2023-02-15 14:19:04 +0600

    cart item added to  localStorage

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -3,19 +3,43 @@ import "./Shop.css";
 import React, { useEffect, useState } from "react";
 import Product from "../Product/Product";
 import Cart from "../Cart/Cart";
+import { addToDb, getStoredCart } from "../../utilities/fakedb";
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState([]);
   useEffect(() => {
+    console.log("products load before fetch");
     fetch("products.json")
       .then((res) => res.json())
-      .then((data) => setProducts(data));
+      .then((data) => {
+        setProducts(data);
+        console.log("products loaded");
+      });
   }, []);
+
+  useEffect(() => {
+    console.log("Local Storage first line");
+    const storedCart = getStoredCart();
+    // console.log(storedCart);
+    const savedCart = [];
+    for (const id in storedCart) {
+      const addedProduct = products.find((product) => product.id === id);
+      if (addedProduct) {
+        const quantity = storedCart[id];
+        addedProduct.quantity = quantity;
+        savedCart.push(addedProduct);
+      }
+      // console.log(addedProduct);
+    }
+    setCart(savedCart);
+    console.log("local storage finished");
+  }, [products]);
 
   const handleAddToCart = (product) => {
     const newCart = [...cart, product];
     setCart(newCart);
+    addToDb(product.id);
   };
 
   return (
