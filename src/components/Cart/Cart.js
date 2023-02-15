99cab6558af4dc295c6fa74e479b64ec293b6cb5
sowmik-sec <sowmik.sec@gmail.com>
2023-02-15 12:28:16 +0600

    showing some cart data

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,10 +2,22 @@ import React from "react";
 import "./Cart.css";
 
 const Cart = ({ cart }) => {
+  let total = 0,
+    shipping = 0;
+  for (const product of cart) {
+    total += product.price;
+    shipping += product.shipping;
+  }
+  const tax = parseFloat((total * 0.1).toFixed(2));
+  const grandTotal = total + shipping + tax;
   return (
-    <div>
+    <div className="cart">
       <h3>Order Summary</h3>
       <p>Selected Items: {cart.length}</p>
+      <p>Total price: ${total}</p>
+      <p>Total Shipping: ${shipping}</p>
+      <p>Tax: {tax.toFixed(2)}</p>
+      <h5>Grand Total: {grandTotal.toFixed(2)}</h5>
     </div>
   );
 };
