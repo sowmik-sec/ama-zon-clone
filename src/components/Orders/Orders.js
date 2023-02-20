b45e0ba2b8f791e30e84c23ceaae719777017ae2
sowmik-sec <sowmik.sec@gmail.com>
2023-02-20 21:03:54 +0600

    remove item handler and make data consistant across UI

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,13 +1,28 @@
 import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import { removeFromDb } from "../../utilities/fakedb";
 import Cart from "../Cart/Cart";
+import ReviewItem from "../ReviewItem/ReviewItem";
 
 const Orders = () => {
   const { products, initialCart } = useLoaderData();
   const [cart, setCart] = useState(initialCart);
+  const handleRemoveItem = (id) => {
+    const remaining = cart.filter((product) => product.id !== id);
+    setCart(remaining);
+    removeFromDb(id);
+  };
   return (
     <div className="shop-container">
-      <div className="products-container"></div>
+      <div className="orders-container">
+        {cart.map((product) => (
+          <ReviewItem
+            key={product.id}
+            product={product}
+            handleRemoveItem={handleRemoveItem}
+          />
+        ))}
+      </div>
       <div className="cart-container">
         <Cart cart={cart} />
       </div>
