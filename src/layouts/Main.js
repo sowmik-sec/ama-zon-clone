3e3e4e66a5a5ce1bb79820220b98d965720254f8
sowmik-sec <sowmik.sec@gmail.com>
2023-02-20 16:05:46 +0600

    import bug fixed and some components added

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Header from "./components/Header/Header";
-import Shop from "./components/Shop/Shop";
+import Header from "../components/Header/Header";
+import Shop from "../components/Shop/Shop";
 
 const Main = () => {
   return (
