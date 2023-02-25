72bdc4f33004d19b6b8a9fc0b320e1311aa0e9a2
sowmik-sec <sowmik.sec@gmail.com>
2023-02-25 22:15:54 +0600

    setup auth context under userContext to share auth info

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,7 +5,7 @@ import "./Login.css";
 const Login = () => {
   return (
     <div className="form-container">
-      <h2 className="form-title">Please login</h2>
+      <h2 className="form-title">Login</h2>
       <form>
         <div className="form-control">
           <label htmlFor="email">Email</label>
